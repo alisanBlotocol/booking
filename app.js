1eaa3e2119d37d7d4789614ada84786a6cdc4b24
alisanBlotocol <alisan.mocadi@yahoo.com>
2019-05-02 16:16:20 +0800

    Added isAuth
    authentication added

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require ('express');
 const bodyParser = require ('body-parser');
 const graphqlHttp = require ('express-graphql');
 const mongoose = require ('mongoose');
-const app = express();
-
 const graphQlSchema = require ('./graphql/schema/index');
 const graphQlResolvers = require ('./graphql/resolvers/index');
+const isAuth = require('./middleware/is-auth');
+
+const app = express();
 
 app.use(bodyParser.json());
+
+app.use(isAuth);
 
 app.use(
     '/graphql',
